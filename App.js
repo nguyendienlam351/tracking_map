d779a3ffc6e139b495ad737f92a6653b45440672
nguyendienlam351 <nguyendienlam351@gmail.com>
2022-06-09 09:03:07 +0700

    tracking map

--- App.js
+++ App.js
@@ -1,31 +1,46 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { SafeAreaView, StyleSheet, Dimensions, PermissionsAndroid, Platform } from 'react-native';
-import MapView, { Marker, PROVIDER_GOOGLE, Polyline } from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Dimensions,
+  PermissionsAndroid,
+  Platform,
+  Text,
+  TouchableOpacity,
+  Alert,
+  View,
+  Linking,
+  Image
+} from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import Geolocation from 'react-native-geolocation-service';
 
 
 const { width, height } = Dimensions.get('window')
 const MAP_KEY = "AIzaSyAQdw23mRj6Bz7Dy1noAEM6p2Sx0IYBr3E"
-
-const ASPECT_RATIO = width / height;
-const LATITUDE_DELTA = 0.004;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const compassImage = require("./images/compass.png")
+const ASPECT_RATIO = width / height
+const LATITUDE_DELTA = 0.004
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
 
 const App = () => {
-  const [state, setState] = useState({
+  const [location, setLocation] = useState({
     curLoc: { // Vị trí bắt đầu
-      latitude: 10.801913,
-      longitude: 106.764748,
+      latitude: 0,
+      longitude: 0,
     },
-    droplocationCords: { // Vị trí kết thúc
-      latitude: 10.815623,
-      longitude: 106.780685,
-    }
+    distance: 0, // Quảng đường còn lại
+    duration: 0, // Thời gian còn lại
   })
-  const { curLoc, droplocationCords } = state
+  const { curLoc, distance, duration } = location
+  const [isFollowUser, setIsFollowUser] = useState(true)
 
-  const mapRef = useRef()
+  // Vị trí kết thúc
+  const [droplocationCords, setdroplocationCords] = useState({
+    latitude: 0,
+    longitude: 0,
+  })
 
   // Yêu cầu cấp quyền
   const locationPermission = () => new Promise(async (resolve, reject) => {
@@ -33,11 +48,11 @@ const App = () => {
       try {
         const permissionStatus = await Geolocation.requestAuthorization('whenInUse');
         if (permissionStatus === 'granted') {
-          return resolve("granted");
+          return resolve("granted")
         }
-        reject('Permission not granted');
+        reject('Permission not granted')
       } catch (error) {
-        return reject(error);
+        return reject(error)
       }
     }
     return PermissionsAndroid.request(
@@ -46,12 +61,12 @@ const App = () => {
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
         resolve("granted");
       }
-      return reject('Location Permission denied');
+      return reject('Location Permission denied')
     }).catch((error) => {
-      console.log('Ask Location permission error: ', error);
-      return reject(error);
-    });
-  });
+      console.log('Ask Location permission error: ', error)
+      return reject(error)
+    })
+  })
 
   // Lấy vị trí hiện tại
   const getCurrentPosition = () => {
@@ -61,9 +76,8 @@ const App = () => {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude
         };
-
-        setState({
-          ...state,
+        setLocation({
+          ...location,
           curLoc: cords
         })
       },
@@ -88,8 +102,8 @@ const App = () => {
           longitude: position.coords.longitude
         };
 
-        setState({
-          ...state,
+        setLocation({
+          ...location,
           curLoc: cords
         })
       },
@@ -106,11 +120,44 @@ const App = () => {
     )
   }
 
+  const setDroplocation = (coordinate) => {
+    Alert.alert(
+      "Thông báo",
+      "Bắt đầu chỉ đường",
+      [
+        {
+          text: "Cancel",
+          style: "cancel"
+        },
+        {
+          text: "OK", onPress: () => {
+            setdroplocationCords(coordinate)
+          }
+        }
+      ]
+    )
+  }
+
   const getLiveLocation = async () => {
     const locPermissionDenied = await locationPermission()
     if (locPermissionDenied) {
-      getCurrentPosition()
       getWatchPosition()
+    }
+  }
+
+  // Chuyển sang google map
+  const handleNavigateGoogleMap = async () => {
+    const url = "https://www.google.com/maps/dir/?api=1"
+      + "&origin=" + curLoc.latitude + "," + curLoc.longitude
+      + "&destination=" + droplocationCords.latitude + "," + droplocationCords.longitude
+      + "&dir_action=navigate&travelmode=driving"
+
+    const supported = await Linking.canOpenURL(url)
+
+    if (supported) {
+      await Linking.openURL(url);
+    } else {
+      Alert.alert(`Don't know how to open this URL: ${url}`)
     }
   }
 
@@ -121,36 +168,75 @@ const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <MapView
-        ref={mapRef}
         style={StyleSheet.absoluteFill}
         showsUserLocation={true}
         showsMyLocationButton={true}
         zoomControlEnabled={true}
         provider={PROVIDER_GOOGLE}
-        followsUserLocation={true}
-        region={{
+        followsUserLocation={isFollowUser}
+        region={isFollowUser ? {
           ...curLoc,
           latitudeDelta: LATITUDE_DELTA,
           longitudeDelta: LONGITUDE_DELTA,
+        } : null}
+        onRegionChange={(Region, isGesture) => {
+          // Nếu lướt trên map sẽ tắt follow theo vị trí user
+          if (isGesture.isGesture) {
+            setIsFollowUser(false)
+          }
+        }}
+        onLongPress={(event) => {
+          const { coordinate } = event.nativeEvent
+          setDroplocation(coordinate)
         }}
       >
-        {/* <Marker
-          ref={markerRef}
-          coordinate={curLoc}
-        /> */}
-        <Marker
-          coordinate={droplocationCords}
-        />
-        <MapViewDirections
-          origin={curLoc}
-          destination={droplocationCords}
-          apikey={MAP_KEY}
-          strokeWidth={5}
-          strokeColor="skyblue"
-          optimizeWaypoints={true}
-          resetOnChange={false}
-        />
+        {
+          (droplocationCords.latitude !== 0 && droplocationCords.longitude !== 0) &&
+          <>
+            <Marker
+              coordinate={droplocationCords}
+            />
+
+            <MapViewDirections
+              origin={curLoc}
+              destination={droplocationCords}
+              apikey={MAP_KEY}
+              strokeWidth={5}
+              strokeColor="skyblue"
+              optimizeWaypoints={true}
+              resetOnChange={false}
+              onReady={({ distance, duration }) => {
+                setLocation({ // Cập nhật quảng đường và thời gian
+                  ...location,
+                  distance: distance,
+                  duration: duration
+                })
+              }}
+            />
+          </>
+        }
       </MapView>
+
+      <View style={[styles.view, { top: 20, alignSelf: "center" }]}>
+        <Text style={styles.text}>
+          {`Khoảng cách còn lại: ${distance.toFixed(1)} km\nThời gian còn lại: ${duration.toFixed(0)} phút`}
+        </Text>
+      </View>
+
+      {
+        (droplocationCords.latitude !== 0 && droplocationCords.longitude !== 0) &&
+        <TouchableOpacity
+          style={[styles.view, { left: 20, bottom: 20 }]}
+          onPress={() => handleNavigateGoogleMap()} >
+          <Image source={compassImage} style={styles.image} />
+        </TouchableOpacity>
+      }
+
+      <TouchableOpacity
+        onPress={() => setIsFollowUser(true)}
+        style={[styles.view, { bottom: 20, alignSelf: "center" }]}>
+        <Text style={styles.text}>Về Giữa</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -158,6 +244,24 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  view: {
+    position: "absolute",
+    backgroundColor: "white",
+    borderRadius: 15,
+    borderWidth: 1
+  },
+  text: {
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    fontWeight: "700",
+    color: "black",
+    fontSize: 15
+  },
+  image: {
+    height: 25,
+    width: 25,
+    margin: 10
   }
 });
 
